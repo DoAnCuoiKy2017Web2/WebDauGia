960089358b87626d5f3ced812c3b41677a36b452
1460202 <nguyenduy2496@outlook.com.vn>
2017-05-11 22:02:40 +0700

    Category
    Hiển thị Danh mục + Sản phẩm theo từng Danh mục.

---
+++ WebDauGia/WebDauGia/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDauGia.Models;
+
+namespace WebDauGia.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                List<Category> list = ctx.Categories.ToList();
+                return PartialView("ListPartial", list); ////// tên Partial
+            }
+        }
+    }
+}
---
+++ WebDauGia/WebDauGia/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDauGia.Models;
+
+namespace WebDauGia.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ByCat(int? id, int page = 1)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                @ViewBag.curPage = page;
+                string c = ctx.Categories
+                    .Where(p => p.CatID == id)
+                    .FirstOrDefault()
+                    .CatName
+                    .ToString();
+                @ViewBag.CatName = c;
+
+                int n = ctx.Products.Where(p => p.CatID == id).Count();
+
+                int recordsPerPage = 3;
+                int nPages = n / recordsPerPage + (n % recordsPerPage == 0 ? 0 : 1);
+
+                @ViewBag.Pages = nPages;
+
+                List<Product> list = ctx.Products
+                    .Where(p => p.CatID == id)
+                    .OrderBy(p => p.ProID)
+                    .Skip((page - 1) * recordsPerPage)
+                    .Take(recordsPerPage)
+                    .ToList();
+
+                return View(list);
+            }
+        }
+    }
+}
