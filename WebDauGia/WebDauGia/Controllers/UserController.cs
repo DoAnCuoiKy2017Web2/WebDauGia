076be9e139a0b32a2aac03db808a85c09ce0f16b
1460202 <nguyenduy2496@outlook.com.vn>
2017-05-12 16:43:53 +0700

    Controler User: Thêm Sửa Xóa
    Chưa làm View

--- WebDauGia/WebDauGia/Controllers/UserController.cs
+++ WebDauGia/WebDauGia/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDauGia.Models;
 
 namespace WebDauGia.Controllers
 {
@@ -13,5 +14,93 @@ namespace WebDauGia.Controllers
         {
             return View();
         }
+
+        //Get : User/Add
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //Post : User/Add
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Add(User model)
+        {
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                ctx.Entry(model).State = System.Data.Entity.EntityState.Added;
+                ctx.SaveChanges();
+                @ViewBag.Message = "Đã thêm thành công.";
+            }
+            return View();
+        }
+
+        //Get : User/Edit
+        public ActionResult Edit(string? ID)
+        {
+            if (ID.HasValue == false)
+            {
+                return RedirectToAction("Index", "User");
+            }
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+
+                User sp = dt.Users
+                    .Where(p => p.UserName == ID.ToString())
+                    .FirstOrDefault();
+                if (sp != null)
+                {
+                    return View(sp);
+                }
+                return RedirectToAction("Index", "User");
+            }
+        }
+
+        //Post: User/Update
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Update(User model)
+        {
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                ctx.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                @ViewBag.Message = "Cập nhật thành công.";
+            }
+            return RedirectToAction("Edit", "User", new { ID = model.UserName });
+        }
+
+        //Get : User/Delete
+        public ActionResult Delete(string? ID)
+        {
+            if (ID.HasValue == false)
+            {
+                return RedirectToAction("Index", "User");
+            }
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+
+                User us = dt.Users
+                    .Where(p => p.UserName == ID.ToString())
+                    .FirstOrDefault();
+                return View(us);
+            }
+        }
+
+        //Post : User/Deleted
+        [HttpPost]
+        public ActionResult Deleted(string ID)
+        {
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                User us = ctx.Users
+                    .Where(p => p.UserName == ID)
+                    .FirstOrDefault();
+                ctx.Entry(us).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("Index", "User");
+        }
+
     }
 }
