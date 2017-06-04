87686e88909de99cb25bec50f1fae01d83f60d78
1460052 <trinhthinh279@gmail.com>
2017-06-05 00:17:49 +0700

    Cookie

---
+++ WebDauGia/WebDauGia/Filters/CheckLoginAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDauGia.Helper;
+
+namespace WebDauGia.Filters
+{
+    public class CheckLoginAttribute : ActionFilterAttribute
+    {
+        public int RequiredPermission { get; set; }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (CurrentContext.IsLogged() == false)
+            {
+                //string controller = filterContext.RouteData.Values["controller"].ToString();
+                //string action = filterContext.RouteData.Values["action"].ToString();
+
+                //filterContext.Result = new RedirectResult(string.Format("~/Account/Login?retUrl=/{0}/{1}", controller, action));
+                filterContext.Result = new RedirectResult("~/User/Login");
+                return;
+            }
+
+            //if (CurrentContext.GetCurUser().f_Permission < this.RequiredPermission)
+            //{
+            //    filterContext.Result = new HttpUnauthorizedResult();
+            //    return;
+            //}
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
