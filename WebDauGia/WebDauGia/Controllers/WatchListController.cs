79af7d0a6b77a0534b4a7c0bc57d849b19f24ba6
VuongNguyen <dragonblue789@gmail.com>
2017-06-09 01:18:00 +0700

    Hoàn Thiện Chức Năng Thêm 1 Sản Phẩm Vào Danh Sách Yêu Thích

--- WebDauGia/WebDauGia/Controllers/WatchListController.cs
+++ WebDauGia/WebDauGia/Controllers/WatchListController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebDauGia.Filters;
 using WebDauGia.Helper;
+using WebDauGia.Models;
 namespace WebDauGia.Controllers
 {
     public class WatchListController : Controller
@@ -19,9 +20,28 @@ namespace WebDauGia.Controllers
         {
             if(CurrentContext.IsLogged()==false)
             {
-                return Json("Vui Lòng Đăng Nhập Để thực hiện chức năng này", JsonRequestBehavior.AllowGet);
+                return Json("1", JsonRequestBehavior.AllowGet);
             }
-            return Json("Muốn Gì Mày",JsonRequestBehavior.AllowGet);
+            else
+            {
+                string username = ((User)Session["user"]).UserName;
+                using(var ctx=new QuanLyDauGiaEntities())
+                {
+                    var wl = ctx.WatchLists.Where(w => w.ProID == ProID && w.UserName == username).FirstOrDefault();
+                    if (wl == null)
+                    {
+                        var wlnew = new WatchList();
+                        wlnew.UserName = username;
+                        wlnew.ProID = ProID;
+                        ctx.WatchLists.Add(wlnew);
+                        ctx.SaveChanges();
+                        return Json("Đã Thêm Vào Danh Sách Yêu Thích!!!", JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                return Json("Sản Phẩm Này Đã Tồn Tại Trong List Của Bạn Rồi!!!", JsonRequestBehavior.AllowGet);
+
+                }
+            }
         }
     }
 }
