0c06601a56a330fd847806e196348061d63d4a77
1460052 <trinhthinh279@gmail.com>
2017-06-08 21:29:06 +0700

    merger

---
+++ WebDauGia/WebDauGia/Controllers/WatchListController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDauGia.Filters;
+using WebDauGia.Helper;
+namespace WebDauGia.Controllers
+{
+    public class WatchListController : Controller
+    {
+        // GET: WatchList
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(int ProID)
+        {
+            if(CurrentContext.IsLogged()==false)
+            {
+                return Json("Vui Lòng Đăng Nhập Để thực hiện chức năng này", JsonRequestBehavior.AllowGet);
+            }
+            return Json("Muốn Gì Mày",JsonRequestBehavior.AllowGet);
+        }
+    }
+}
