80913947a022fac70f46ace495c938aed15cad28
1460202 <nguyenduy2496@outlook.com.vn>
2017-06-09 10:41:07 +0700

    Hiện chi tiết

--- WebDauGia/WebDauGia/Models/Function.cs
+++ WebDauGia/WebDauGia/Models/Function.cs
@@ -24,6 +24,22 @@ namespace WebDauGia.Models
             }
             return check;
         }
+        public static double GetReliability(String username)
+        {
+            double diemdanhgia = 0;
+            using (var ctx = new QuanLyDauGiaEntities())
+            {
+                User us = ctx.Users
+                    .Where(p => p.UserName == username)
+                    .FirstOrDefault();
+                if(us != null)
+                {
+                    String []diemdanhgiaArr = us.Reliability.Split('/');
+                    diemdanhgia = double.Parse(diemdanhgiaArr[0]) / (double.Parse(diemdanhgiaArr[0]) + double.Parse(diemdanhgiaArr[1]));
+                }
+            }
+            return diemdanhgia * 100;
+        }
     }
     
 
