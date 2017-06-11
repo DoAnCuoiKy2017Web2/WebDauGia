32accbe88157c118b9fe7fcb125d4b9d739311ba
VuongNguyen <dragonblue789@gmail.com>
2017-06-11 12:05:47 +0700

    Hoàn Thành Chức Năng Bán Hàng.
    Chưa  tự động load from.
    để test cần phải  chuyển ownerPrice về 0 thay vì để null nhé.

--- WebDauGia/WebDauGia/Controllers/AuctionHistoryController.cs
+++ WebDauGia/WebDauGia/Controllers/AuctionHistoryController.cs
@@ -25,11 +25,7 @@ namespace WebDauGia.Controllers
         //[ValidateInput(false)]
         public ActionResult Add(double giatra, int proid)
         {
-            //AuctionHistory a = new AuctionHistory();
-            //a.ProID = proid;
-            //a.UserName = uname;
-            //a.Time = time;
-            //a.AucPrice = ragia;
+            int TH = 0;
             using (var ctx = new QuanLyDauGiaEntities())
             {
                 var pro = ctx.Products.Where(p => p.ProID == proid).FirstOrDefault();
@@ -40,9 +36,39 @@ namespace WebDauGia.Controllers
                 auhis.Time = DateTime.Now;
                 ctx.AuctionHistorys.Add(auhis);
                 ctx.SaveChanges();
+                //tang lượt đâu giá lên 1
+                pro.NumOfAuction += 1;
+                if(giatra >=pro.Price)
+                {
+                    TH = 3;//chiến thắng tuyệt đối
+                    //update Product
+                    pro.OwnerPrice = pro.Price;
+                    pro.AucPrice = pro.Price;
+                    pro.EndTime = DateTime.Now;
+                    ctx.Entry(pro).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+                else if(giatra > pro.OwnerPrice)
+                {
+                    TH=1;// 1 là chiến thắng.
+                    //update Product
+                    pro.OwnerPrice = giatra;
+                    pro.AucPrice += pro.StepPrice;
+                    pro.Owner = ((User)Session["user"]).UserName;
+                    ctx.Entry(pro).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+                else if(giatra <= pro.OwnerPrice)
+                {
+                    TH = 2;//2 la thua 
+                    //update Product
+                    pro.AucPrice = giatra;
+                    ctx.Entry(pro).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                }
             }
             //return RedirectToAction("Detail", "Product", new { ID = a.ProID });
-            return Json(giatra, JsonRequestBehavior.AllowGet);
+            return Json(TH, JsonRequestBehavior.AllowGet);
         }
 
         ////Post : AuctionHistory/Add
