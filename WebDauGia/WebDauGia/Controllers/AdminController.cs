34800944fde8397f027871c9870b90a6095952a3
1460224 <duongminhminh96@gmail.com>
2017-06-12 16:50:28 +0700

    Cập nhật chức năng của admin
    xóa sửa người dùng, xóa sửa danh mục( chưa kiểm tra danh mục có tồn tại sản phẩm)

--- WebDauGia/WebDauGia/Controllers/AdminController.cs
+++ WebDauGia/WebDauGia/Controllers/AdminController.cs
@@ -44,5 +44,149 @@ namespace WebDauGia.Controllers
                 return View();
             }
         }
+
+        // GET: Admin/Login
+        public ActionResult RemoveUser()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult RemoveUser(string proId)
+        {
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+                User us = dt.Users
+                    .Where(p => p.UserName == proId)
+                    .FirstOrDefault();
+                if (us != null)
+                {
+                    dt.Entry(us).State = System.Data.Entity.EntityState.Deleted;
+                    dt.SaveChanges();
+                    @ViewBag.Message = "Cập nhật thành công.";
+
+                    Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi.')</script>");
+                    return RedirectToAction("ManageUser", "Admin");
+                }
+                Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi')</script>");
+                return View();
+            }
+        }
+        // GET: Admin/Login
+        public ActionResult UpdateUserPassword()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult UpdateUserPassword(string proId,string newpass)
+        {
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+                User us = dt.Users
+                    .Where(p => p.UserName == proId)
+                    .FirstOrDefault();
+                if (us != null)
+                {
+                    us.Password = StringUtils.MD5(newpass);
+                    dt.Entry(us).State = System.Data.Entity.EntityState.Modified;
+                    dt.SaveChanges();
+                    @ViewBag.Message = "Cập nhật thành công.";
+
+                    Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi.')</script>");
+                    return RedirectToAction("ManageUser", "Admin");
+                }
+                Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi')</script>");
+                return View();
+            }
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult Test(string proId)
+        {
+            return View();
+        }
+
+        public ActionResult ManageUser()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult ManageUser(string proId)
+        {
+            return View();
+        }
+        public ActionResult ManageCategory()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult ManageCategory(string proId)
+        {
+            return View();
+        }
+
+        public ActionResult RemoveCat()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult RemoveCat(string proId)
+        {
+            int id = int.Parse(proId);
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+                Category us = dt.Categories
+                    .Where(p => p.CatID == id)
+                    .FirstOrDefault();
+                if (us != null)
+                {
+                    dt.Entry(us).State = System.Data.Entity.EntityState.Deleted;
+                    dt.SaveChanges();
+                    @ViewBag.Message = "Cập nhật thành công.";
+
+                    Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi.')</script>");
+                    return RedirectToAction("ManageCategory", "Admin");
+                }
+                Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi')</script>");
+                return View();
+            }
+        }
+        // GET: Admin/Login
+        public ActionResult UpdateCat()
+        {
+            return View();
+        }
+        // Post: Admin/Login
+        [HttpPost]
+        public ActionResult UpdateCat(string proId, string newpass)
+        {
+            int id = int.Parse(proId);
+            using (QuanLyDauGiaEntities dt = new QuanLyDauGiaEntities())
+            {
+                Category us = dt.Categories
+                    .Where(p => p.CatID == id)
+                    .FirstOrDefault();
+                if (us != null)
+                {
+                    us.CatName = newpass;
+                    dt.Entry(us).State = System.Data.Entity.EntityState.Modified;
+                    dt.SaveChanges();
+                    @ViewBag.Message = "Cập nhật thành công.";
+
+                    Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi.')</script>");
+                    return RedirectToAction("ManageCategory", "Admin");
+                }
+                Response.Write("<script LANGUAGE='JavaScript' >alert('ahihi')</script>");
+                return View();
+            }
+        }
     }
 }
---
+++ WebDauGia/WebDauGia/Helper/UserX.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDauGia.Helper
+{
+    public class UserX
+    {
+        public List<LUser> Items { get; set; }
+        public UserX()
+        {
+            this.Items = new List<LUser>();
+        }
+
+        //public int SumQ()
+        //{
+        //    return this.Items.Sum(i => i.Quantity);
+        //}
+
+        //public void AddItem(CartItem item)
+        //{
+        //    var eItem = this.Items
+        //        .Where(i => i.Product.ProID == item.Product.ProID)
+        //        .FirstOrDefault();
+
+        //    if (eItem != null)
+        //    {
+        //        eItem.Quantity += item.Quantity;
+        //    }
+        //    else
+        //    {
+        //        this.Items.Add(item);
+        //    }
+
+        //}
+
+        //public void RemoveItem(int proId)
+        //{
+        //    var toDeleteItem = this.Items
+        //       .Where(i => i.Product.ProID == proId)
+        //       .FirstOrDefault();
+
+        //    if (toDeleteItem != null)
+        //    {
+        //        this.Items.Remove(toDeleteItem);
+        //    }
+        //}
+        //public void UpdateItem(int proId, int quantity)
+        //{
+        //    var toUpdateItem = this.Items
+        //       .Where(i => i.Product.ProID == proId)
+        //       .FirstOrDefault();
+
+        //    if (toUpdateItem != null)
+        //    {
+        //        toUpdateItem.Quantity = quantity;
+        //    }
+        //}
+    }
+    public class LUser
+    {
+        public UserX UserX { get; set; }
+    }
+}
