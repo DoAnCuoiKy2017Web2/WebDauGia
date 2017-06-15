3de56cf6e07348742f97ae1dd79f101065a3ed78
1460202 <nguyenduy2496@outlook.com.vn>
2017-06-15 09:13:48 +0700

    retore usercontrol
    đăng nhập, ghi nhớ đăng nhập

--- WebDauGia/WebDauGia/Controllers/UserController.cs
+++ WebDauGia/WebDauGia/Controllers/UserController.cs
@@ -23,11 +23,11 @@ namespace WebDauGia.Controllers
         {
             LoginVM model = new LoginVM();
             model.UserName = "";
-            if(Response.Cookies["userID"].Expires > DateTime.Now)
-            {
-                model.UserName = Response.Cookies["userID"].Value;
-                model.PassWord = "***";
-            }
+            //if(Response.Cookies["userID"].Expires > DateTime.Now)
+            //{
+            //    model.UserName = Response.Cookies["userID"].Value;
+            //    model.PassWord = "***";
+            //}
             return View(model);
         }
         // Post: User/Login
@@ -40,12 +40,12 @@ namespace WebDauGia.Controllers
                 User us = dt.Users
                     .Where(p => p.UserName == model.UserName && p.Password == pass)
                     .FirstOrDefault();
-                if (Response.Cookies["userID"].Value == model.UserName && Response.Cookies["userID"].Expires > DateTime.Now)
-                {
-                    us = dt.Users
-                    .Where(p => p.UserName == model.UserName)
-                    .FirstOrDefault();
-                }
+                //if (Response.Cookies["userID"].Value == model.UserName && Response.Cookies["userID"].Expires > DateTime.Now)
+                //{
+                //    us = dt.Users
+                //    .Where(p => p.UserName == model.UserName)
+                //    .FirstOrDefault();
+                //}
                 if (us != null)
                 {
                     if (model.Remember != null)
@@ -54,10 +54,10 @@ namespace WebDauGia.Controllers
                         Response.Cookies["userID"].Value = us.UserName.ToString();
                         Response.Cookies["userID"].Expires = DateTime.Now.AddDays(7);
                     }
-                    else
-                    {
-                        Response.Cookies["userID"].Expires = DateTime.Now.AddDays(-1);
-                    }
+                    //else
+                    //{
+                    //    Response.Cookies["userID"].Expires = DateTime.Now.AddDays(-1);
+                    //}
                     Session["isLogin"] = 1;
                     Session["user"] = us;
                     Session["username"] = us.UserName;
